5a0b58e5e8ba370cf63570a620f0ea017e06886e
Pheeleex <ohemufelix@gmail.com>
2023-12-17 16:47:45 +0100

    another commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,7 +21,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={7} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.35: 0.75}
+        scale={isMobile ? 0.3: 0.75}
         position={isMobile ? [0, -3, -0.56] : [0, -3.8, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
