f269a3e549a82da7b5cb1ec2dde025bd49b4767f
Pheeleex <ohemufelix@gmail.com>
2024-07-20 15:39:06 +0100

    fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,7 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 
-import Apple from "./Appleclone.png"
+import Apple from "./tech/Appleclone.png"
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
